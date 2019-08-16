92563c13f2a78ded2d2d79e24c2a693127f30f1d
ebonight337 <senndayuuki337@yahoo.co.jp>
2019-08-16 14:57:51 +0900

    Update Get-TeamsSite.ps1

--- Get-TeamsSite.ps1
+++ Get-TeamsSite.ps1
@@ -4,15 +4,7 @@
 ###################################################
 Function GetTeamsSite(){
     $Credential = Get-Credential
-    # Teamsに接続
-    try{
-        Write-Host "Teamsへ接続を行います。" -BackgroundColor Green -ForegroundColor Black
-        Install-Module -Name MicrosoftTeams
-        Connect-MicrosoftTeams -Credential $Credential
-    }catch{
-        $ErrorMessage = $_.Exception.Message
-        throw "Failed Connect-Microsoft Teams command. `r`nPlease check the following error. `r`n $($ErrorMessage)"
-    }
+    $ScriptFolder = Join-Path $PSScriptRoot TeamList.csv
 
     # Exchangeに接続
     try{
@@ -21,7 +13,7 @@ Function GetTeamsSite(){
         $SessionId = Import-PSSession $Session -DisableNameChecking
     }catch{
         $ErrorMessage = $_.Exception.Message
-        throw "Failed connect to MicrosoftExchange. `r`nPlease check the following error. `r`n $($ErrorMessage)"
+        throw "MicrosoftExchangeに接続できませんでした。 `r`nエラーを確認してください。 `r`n $($ErrorMessage)"
     }
 
     # チームサイト取得
@@ -33,43 +25,17 @@ Function GetTeamsSite(){
     foreach ($o365group in $o365groups) 
     {
         try{
-            $teamschannels = Get-TeamChannel -GroupId $o365group.ExternalDirectoryObjectId
-            $o365GroupMemberList = (Get-UnifiedGroupLinks -identity $o365group.ExternalDirectoryObjectId -LinkType Members) | select -expandproperty PrimarySmtpAddress
-            $TeamsList = $TeamsList + [pscustomobject]@{GroupId = $o365group.ExternalDirectoryObjectId; GroupName = $o365group.DisplayName; `
-            SMTP = $o365group.PrimarySmtpAddress; Members = $o365group.GroupMemberCount; MemberList = $o365GroupMemberList -join ', '; `
-            SPOSite = $o365group.SharePointSiteUrl; TeamsEnabled = $true; Owners = $o365group.ManagedBy}
-        } 
-        catch{
-            $ErrorCode = $_.Exception.ErrorCode
-            switch ($ErrorCode) 
-            {
-                "404" 
-                {
-                    $TeamsList = $TeamsList + [pscustomobject]@{GroupId = $o365group.ExternalDirectoryObjectId; GroupName = $o365group.DisplayName; `
-                    SMTP = $o365group.PrimarySmtpAddress; Members = $o365group.GroupMemberCount; MemberList = $o365GroupMemberList -join ', '; `
-                    SPOSite = $o365group.SharePointSiteUrl; TeamsEnabled = $false; Owners = $o365group.ManagedBy}
-                    break;
-                }
-                "403" 
-                {
-                    $TeamsList = $TeamsList + [pscustomobject]@{GroupId = $o365group.ExternalDirectoryObjectId; GroupName = $o365group.DisplayName; `
-                    SMTP = $o365group.PrimarySmtpAddress; Members = $o365group.GroupMemberCount; MemberList = $o365GroupMemberList -join ', '; `
-                    SPOSite = $o365group.SharePointSiteUrl; TeamsEnabled = $true; Owners = $o365group.ManagedBy}
-                    break;
-                }
-                default 
-                {
-                    Write-Error ("Unknown ErrorCode trying to 'Get-TeamChannel -GroupId {0}' :: {1}" -f $o365group, $ErrorCode)
-                }
+            # Teamのみ取得
+            if ($o365group.ResourceProvisioningOptions -eq "Team"){
+                $teamsList = $teamsList + [pscustomobject]@{TeamsID = $o365group.PrimarySmtpAddress; TeamsName = $o365group.DisplayName; SharePointUrl = $o365group.SharePointSiteUrl}
             }
+        } catch{
+            $ErrorMessage = $_.Exception.Message
+            throw "Teamが取得できませんでした。 `r`nエラーを確認してください。 `r`n $($ErrorMessage)"
         }
     }
-    Disconnect-MicrosoftTeams
     Get-PSSession | %{Remove-PSSession -Id $_.Id}
-    if(Test-Path "c:\temp" -eq $False){
-        New-Item -ItemType directory -Path c:\temp
-    }
-    $TeamsList | export-csv C:\temp\AllTeamsInTenant.csv -NoTypeInformation -Encoding UTF8
+    $TeamsList | export-csv $ScriptFolder -NoTypeInformation -Encoding UTF8
 
 }
 
